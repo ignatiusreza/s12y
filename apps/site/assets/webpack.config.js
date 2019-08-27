3ccd30ca158bcc4f77f13faec787a3eecabfcd78
Ignatius Reza <lyoneil.de.sire@gmail.com>
2019-08-27 11:57:44 +0900

    initialize tailwindcss

--- apps/site/assets/webpack.config.js
+++ apps/site/assets/webpack.config.js
@@ -45,7 +45,18 @@ module.exports = (env, options) => ({
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"]
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1
+            }
+          },
+          {
+            loader: "postcss-loader"
+          }
+        ]
       }
     ]
   },
